89a0a276f23122e4e6c223ae802381ff41fe5652
Simon Cropp <simon.cropp@gmail.com>
2023-06-04 21:10:12 +1000

    Update VerifySystemJson.cs

--- src/Verify.SystemJson/VerifySystemJson.cs
+++ src/Verify.SystemJson/VerifySystemJson.cs
@@ -2,10 +2,6 @@
 
 public static class VerifySystemJson
 {
-    [Obsolete("Use Initialize")]
-    public static void Enable() =>
-        Initialize();
-
     public static bool Initialized { get; private set; }
 
     public static void Initialize()
