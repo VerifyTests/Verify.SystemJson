6729565309b92061e12db8b21f46c5b137982714
Simon Cropp <simon.cropp@gmail.com>
2024-09-14 19:27:17 +1000

    cleanup

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,17 +1,17 @@
 public class Tests
 {
     string json = """
-        {
-          "short": {
-            "original": "http://www.foo.com/",
-            "short": "foo",
-            "error": {
-              "code": 0,
-              "msg": "No action taken"
-            }
-          }
-        }
-        """;
+                  {
+                    "short": {
+                      "original": "http://www.foo.com/",
+                      "short": "foo",
+                      "error": {
+                        "code": 0,
+                        "msg": "No action taken"
+                      }
+                    }
+                  }
+                  """;
 
     [Fact]
     public Task TestJsonDocument() =>
@@ -31,21 +31,25 @@ public class Tests
 
     [Fact]
     public Task NullValue() =>
-        Verify(JsonDocument.Parse("""
-        {
-          "short": {
-            "a": null,
-            "error": "a"
-          }
-        }
-        """));
+        Verify(
+            JsonDocument.Parse(
+                """
+                {
+                  "short": {
+                    "a": null,
+                    "error": "a"
+                  }
+                }
+                """));
 
     [Fact]
     public Task Numbers() =>
-        Verify(JsonDocument.Parse("""
-        {
-            "int": 1,
-            "double": 4.4
-        }
-        """));
+        Verify(
+            JsonDocument.Parse(
+                """
+                {
+                    "int": 1,
+                    "double": 4.4
+                }
+                """));
 }
