60f55370641366e963ccdcad03087bf99f486572
Simon Cropp <simon.cropp@gmail.com>
2025-06-08 10:44:52 +1000

    Update Tests.cs

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,8 +1,3 @@
-using System.Text.Encodings.Web;
-using System.Text.Unicode;
-using NUnit.Framework.Legacy;
-using JsonSerializer = System.Text.Json.JsonSerializer;
-
 [TestFixture]
 public class Tests
 {
@@ -23,45 +18,6 @@ public class Tests
                     }
                   }
                   """;
-
-#pragma warning disable CA1869
-    [Test]
-    public void EncodingTest()
-    {
-        var value = new
-        {
-            value = "\""
-        };
-        json = JsonSerializer.Serialize(value);
-        ClassicAssert.AreEqual(
-            """
-            {
-              "value": "\u0022"
-            }
-            """,
-            json);
-    }
-    [Test]
-    public void EncodingWithRelaxedTest()
-    {
-        var options = new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true
-        };
-        var value = new
-        {
-            value = "\""
-        };
-        json = JsonSerializer.Serialize(value, options);
-        ClassicAssert.AreEqual(
-            """
-            {
-              "value": "\""
-            }
-            """,
-            json);
-    }
 
     [Test]
     public Task ScrubMember() =>
@@ -94,12 +50,11 @@ public class Tests
                       }
                     }
                     """))
-            .AddExtraSettings(
-                _ =>
-                {
-                    _.DefaultValueHandling = DefaultValueHandling.Include;
-                    _.NullValueHandling = NullValueHandling.Include;
-                });
+            .AddExtraSettings(_ =>
+            {
+                _.DefaultValueHandling = DefaultValueHandling.Include;
+                _.NullValueHandling = NullValueHandling.Include;
+            });
 
     [Test]
     public Task Numbers() =>
